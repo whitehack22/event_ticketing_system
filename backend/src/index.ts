babcb7447f1ffa04e15095caa104e149707b90e7
whitehack22 <mugoryan2@gmail.com>
2025-07-18 05:17:15 +0300

    Created React Application

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,12 +7,17 @@ import venue from "./venues/venue.router";
 import user from "./users/user.router";
 import { logger } from "./middleware/logger";
 import { rateLimiterMiddleware } from "./middleware/rateLimiter";
+import cors from "cors";
 
 const initilizeApp = () => {
 const app = express()
 
 //middleware
 app.use(express.json());
+ app.use(cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  }))
 // app.use(logger)
 // app.use(rateLimiterMiddleware)
 
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,9 @@
 /* istanbul ignore next */
 import app from "./index";
+import dotenv from 'dotenv/config';
 
-app.listen(8081, () => {
-    console.log("Server is running on http://localhost:8081");
+const PORT = process.env.PORT || 8081;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port http://localhost:${PORT}`);
 }) 
